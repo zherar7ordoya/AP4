dc2ff0b804ee5b50f36bbe8074f6c597b4351774
zherar7ordoya <zherar7ordoya@outlook.com>
2022-02-15 13:19:40 -0300

    2022-02-15

---
+++ DelegadosEventos/Delegado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegados
+{
+    // Cada entidad (un delegado es una entidad) debe tener su propia clase.
+    // Un delegado es una variable que contiene una función.
+
+    // Aquí defino qué tipo de funciones puede contener.
+    // En este caso, una void que reciba un parámetro int.
+    public delegate void DelegadoVoidEntero(int entero);
+    public delegate void DelegadoVoidCadena(string cadena);
+}
