64e399a0fce39137806a740484a6eda9ab578e54
zherar7ordoya <zherar7ordoya@outlook.com>
2022-02-18 20:31:22 -0300

    2022-02-18

---
+++ HdeLeon/ModeloNegocio/People.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ModeloNegocio
+{
+    public class People
+    {
+        public static List<Datos.People> Get()
+        {
+            using (Datos.HdeLeonEntities db = new Datos.HdeLeonEntities())
+            {
+                return db.People.ToList();
+            }
+        }
+    }
+}
