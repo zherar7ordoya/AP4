885549a85f04f9570501d76d85af92879ba3f537
zherar7ordoya <zherar7ordoya@outlook.com>
2022-02-19 10:54:24 -0300

    2022-02-19

---
+++ 6) N-Capas (Ejemplos)/SiliconSystem/Utilities/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconSystem.Utilities
+{
+    internal class Logger
+    {
+    }
+}
