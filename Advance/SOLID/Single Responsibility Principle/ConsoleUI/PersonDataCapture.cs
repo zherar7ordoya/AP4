1b2746fe0ee6d046bb22ca8fa87a996f3372984d
zherar7ordoya <zherar7ordoya@outlook.com>
2022-02-24 12:53:28 -0300

    2022-02-24

---
+++ Advance/SOLID/Single Responsibility Principle/ConsoleUI/PersonDataCapture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleUI
+{
+    public class PersonDataCapture
+    {
+        public static Person Capture()
+        {
+            Person output = new Person();
+
+            Write("First name: ");
+            output.FirstName = ReadLine();
+
+            Write("Last name: ");
+            output.LastName = ReadLine();
+
+            return output;
+        }
+    }
+}
---
+++ Advance/SOLID/Single Responsibility Principle/ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleUI;
+
+StandardMesssages.WelcomeMessage();
+
+Person user = PersonDataCapture.Capture();
+
+bool isUserValid = PersonValidator.Validate(user);
+
+if (isUserValid is false)
+{
+    StandardMesssages.EndApplication();
+    return;
+}
+
+AccountGenerator.CreateAccount(user);
+
+StandardMesssages.EndApplication();
