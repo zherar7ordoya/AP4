f23bef39d62c69840d11c5d72140d0e510993dd8
zherar7ordoya <zherar7ordoya@outlook.com>
2022-03-04 13:07:16 -0300

    2022-03-04

---
+++ Advance/9) LINQ/Nicosio/01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace _01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pares = ClaseExplicita.ObtenerNumerosPares();
+            var postres = ClaseExplicita.ObtenerPostres();
+            var impares = ClaseExplicita.ObtenerNumerosImpares();
+
+            WriteLine("\nPARES");
+            WriteLine(string.Join(",\n", pares));
+            WriteLine("\nPOSTRES");
+            WriteLine(string.Join(",\n", postres));
+            WriteLine("\nIMPARES");
+            WriteLine(string.Join(",\n", impares));
+
+            //*-------------------------*
+            ReadKey();
+        }
+    }
+}
