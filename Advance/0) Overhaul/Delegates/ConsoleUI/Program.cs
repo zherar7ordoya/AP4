ec96cf5e197c799d1ed3de0aa36fd20b1e69b8e3
zherar7ordoya <zherar7ordoya@outlook.com>
2022-03-08 22:39:35 -0300

    2022-03-08

---
+++ Advance/0) Overhaul/Delegates/ConsoleUI/Program.cs
@@ -0,0 +1,39 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static ShoppingCartModel cart = new ShoppingCartModel();
+
+        static void Main(string[] args)
+        {
+            PopulateCartWithDemoData();
+
+            WriteLine($"The total for the cart is {cart.GenerateTotal(SubTotalAlert):C2}");
+
+            Console.WriteLine();
+            Console.Write("Please press any key to exit the application...");
+            Console.ReadKey();
+        }
+
+        private static void PopulateCartWithDemoData()
+        {
+            cart.Items.Add(new ProductModel { ItemName = "Cereal", Price = 3.63M });
+            cart.Items.Add(new ProductModel { ItemName = "Milk", Price = 2.95M });
+            cart.Items.Add(new ProductModel { ItemName = "Strawberries", Price = 7.51M });
+            cart.Items.Add(new ProductModel { ItemName = "Blueberries", Price = 8.84M });
+        }
+
+        private static void SubTotalAlert(decimal subTotal)
+        {
+            WriteLine($"The subtotal is {subTotal:C2}");
+        }
+    }
+}
---
+++ Advance/0) Overhaul/Delegates/DemoLibrary/ShoppingCartModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class ShoppingCartModel
+    {
+        public delegate void MentionDiscount(decimal subTotal);
+
+        public List<ProductModel> Items { get; set; } = new List<ProductModel>();
+        
+        public decimal GenerateTotal(MentionDiscount mentionDiscount)
+        {
+            decimal subTotal = Items.Sum(x => x.Price);
+            mentionDiscount(subTotal);
+
+            if (subTotal > 100) return subTotal * 0.80M;
+            else if (subTotal > 50) return subTotal * 0.85M;
+            else if (subTotal > 10) return subTotal * 0.90M;
+            else return subTotal;
+        }
+    }
+}
