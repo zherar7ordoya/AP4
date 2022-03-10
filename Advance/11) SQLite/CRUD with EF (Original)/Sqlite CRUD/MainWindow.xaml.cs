4a86e162c2da5c8b7e564403f0fb4cc1df3499bb
zherar7ordoya <zherar7ordoya@outlook.com>
2022-03-10 13:08:02 -0300

    2022-03-10

--- Advance/11) SQLite/CRUD with EF (Original)/Sqlite CRUD/MainWindow.xaml.cs
+++ Advance/11) SQLite/CRUD with EF (Original)/Sqlite CRUD/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Sqlite_CRUD
 {
@@ -21,15 +10,14 @@ namespace Sqlite_CRUD
     public partial class MainWindow : Window
     {
         public List<User> DatabaseUsers { get; private set; }
-
         public MainWindow()
         {
             InitializeComponent();
+            Read();
         }
 
         public void Create()
         {
-
             using (DataContext context = new DataContext())
             {
                 var name = NameTextBox.Text;
@@ -40,99 +28,58 @@ namespace Sqlite_CRUD
                     context.Users.Add(new User() { Name = name, Address = address });
                     context.SaveChanges();
                 }
-
             }
+            Read();
         }
 
         public void Read()
         {
             using (DataContext context = new DataContext())
-            {               
-                 DatabaseUsers = context.Users.ToList();
+            {
+                DatabaseUsers = context.Users.ToList();
                 ItemList.ItemsSource = DatabaseUsers;
             }
-
         }
 
         public void Update()
         {
-
-
             using (DataContext context = new DataContext())
             {
-
-                User selectedUser =   ItemList.SelectedItem as User;
-
+                User selectedUser = ItemList.SelectedItem as User;
                 var name = NameTextBox.Text;
                 var address = AddressTextBox.Text;
 
                 if (name != null && address != null)
                 {
-
-                     User user = context.Users.Find(selectedUser.Id);
+                    User user = context.Users.Find(selectedUser.Id);
                     user.Name = name;
                     user.Address = address;
-
                     context.SaveChanges();
                 }
-
             }
-
-
-
+            Read();
         }
 
         public void Delete()
         {
-
-
             using (DataContext context = new DataContext())
             {
-
                 User selectedUser = ItemList.SelectedItem as User;
 
                 if (selectedUser != null)
                 {
-                    User user = context.Users.Single(x=> x.Id == selectedUser.Id);
-
+                    User user = context.Users.Single(x => x.Id == selectedUser.Id);
                     context.Remove(user);
                     context.SaveChanges();
-
                 }
-
-            
-
             }
-
-
-
-        }
-
-        private void CreateButton_Click(object sender, RoutedEventArgs e)
-        {
-            Create();
-        }
-
-        private void ReadButton_Click(object sender, RoutedEventArgs e)
-        {
             Read();
         }
 
-        private void UpdateButton_Click(object sender, RoutedEventArgs e)
-        {
-            Update();
-        }
-
-        private void DeleteButton_Click(object sender, RoutedEventArgs e)
-        {
-            Delete();
-        }
-
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-
-            ItemList.Items.Clear();
-
-        }
+        private void CreateButton_Click(object sender, RoutedEventArgs e) => Create();
+        private void ReadButton_Click(object sender, RoutedEventArgs e) => Read();
+        private void UpdateButton_Click(object sender, RoutedEventArgs e) => Update();
+        private void DeleteButton_Click(object sender, RoutedEventArgs e) => Delete();
+        private void MenuItem_Click(object sender, RoutedEventArgs e) => ItemList.Items.Clear();
     }
 }
