bc1a0be0d0cd4ee7275376c5459eff022198c8c1
zherar7ordoya <zherar7ordoya@outlook.com>
2022-03-14 23:36:43 -0300

    2022-03-14

---
+++ Advance/10) Delegados & Eventos/Cycling Club Registration System/ClubMembership/FieldValidators/FieldConstants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembership.FieldValidators
+{
+    public class FieldConstants
+    {
+        public enum UserRegistrationField
+        {
+            EmailAddress,
+            Firstname,
+            Lastname,
+            Password,
+            PasswordCompare,
+            DateOfBirth,
+            PhoneNumber,
+            AddressFirstLine,
+            AddressSecondLine,
+            AddressCity,
+            PostCode
+        }
+    }
+}
---
+++ Advance/10) Delegados & Eventos/Cycling Club Registration System/ClubMembership/FieldValidators/IFieldValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembership.FieldValidators
+{
+    public delegate bool FieldValidatorDel(
+        int fieldIndex,
+        string fieldValue,
+        string[] fieldArray,
+        out string fieldInvalidMessage);
+
+    public interface IFieldValidator
+    {
+        void InitialiseValidatorDelegates();
+        string[] FieldArray { get; }
+        FieldValidatorDel ValidatorDel { get; }
+    }
+}
