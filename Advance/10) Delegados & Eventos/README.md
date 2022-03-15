46402ef28136c839f6bfe182d8c91925308a0886
zherar7ordoya <zherar7ordoya@outlook.com>
2022-03-15 20:00:08 -0300

    2022-03-15

---
+++ Advance/10) Delegados & Eventos/README.md
@@ -0,0 +1,7 @@
+# LECTURAS PENDIENTES
+
+- [IObservable<T> Interface](https://docs.microsoft.com/en-us/dotnet/api/system.iobservable-1?view=net-6.0)
+- [IObserver<T> Interface](https://docs.microsoft.com/en-us/dotnet/api/system.iobserver-1?view=net-6.0)
+- [Observer Design Pattern](https://docs.microsoft.com/en-us/dotnet/standard/events/observer-design-pattern)
+- [Handle and raise events](https://docs.microsoft.com/en-us/dotnet/standard/events/)
+- [How to: Raise and Consume Events](https://docs.microsoft.com/en-us/dotnet/standard/events/how-to-raise-and-consume-events)
---
+++ Advance/10) Delegados & Eventos/Tipos Covariantes y Contravariantes/Program.cs
@@ -0,0 +1,91 @@
+﻿using static System.Console;
+using System;
+using System.Collections.Generic;
+
+namespace CovariantesContravariantes
+{
+    internal class Program
+    {
+        /** PRELIMINARES
+         * Los delegados Action y Func toman in y out:
+         * -Action: in
+         * -Func: out
+         */
+        static void Main(string[] args)
+        {
+            // COVARIANZA
+            // Asignando clase mayor a clase menor.
+            Vehicle v1 = new Car();     //Todos los autos son vehículos...
+            Vehicle v2 = new Car();
+
+            Vehicle v3 = new Aircraft();//pero no todos los vehículos son autos.
+
+            Func<Vehicle> func = GetVehicle;
+            Vehicle v4 = func();
+
+            // CONTRAVARIANZA
+            // Asignando clase menor a clase mayor.
+            Action<Car> action = ParkVehicle;
+            action(new Car());
+
+            // EN PARÁMETROS DE TIPO GENÉRICO
+            ICovariant<Vehicle> covariant1 = (ICovariant<Vehicle>)null;
+
+            // Asignando más a menos (covarianza).
+            ICovariant<Vehicle> covariant2 = (ICovariant<Car>)null;
+
+            IContravariant<Vehicle> contravariant1 = (IContravariant<Vehicle>)null;
+
+            // Asignando menos a más (contravarianza).
+            IContravariant<Car> contravariant2 = (IContravariant<Vehicle>)null;
+
+            /** POSTLIMINAR
+             * Los parámetros Action de entrada son contravariantes.
+             * Los parámetros Func de entrada son contravariantes.
+             * Los parámetros Func de salida son covariantes.
+             * 
+             * Para finalizar, un ejemplo del mundo real.
+             */
+            var sorted = new SortedSet<Car>(new VehicleComparer())
+            {
+                new Car() { Speed=150 },
+                new Car() { Speed=120 },
+                new Car() { Speed=170 },
+
+            };
+            foreach (var item in sorted) { WriteLine("Speed; {0}", item.Speed); }
+            ReadKey();
+        }
+
+        static Car GetVehicle() => new Car();
+        static void ParkVehicle(Vehicle vehicle) { }
+    }
+
+    #region CLASES
+    class Vehicle { public int Speed { get; set; } }
+    class Car : Vehicle { }
+    class Aircraft : Vehicle { }
+
+    class VehicleComparer : IComparer<Vehicle>
+    {
+        public int Compare(Vehicle x, Vehicle y)
+        {
+            return x.Speed - y.Speed;
+        }
+    }
+    #endregion
+
+    // OUT significa que estamos generando objetos desde esta interfaz.
+    interface ICovariant<out T>
+    {
+        T Name { get; } // Solo getters.
+        T Get();
+    }
+
+    // IN significa que estamos recibiendo objetos desde esta interfaz.
+    interface IContravariant<in T>
+    {
+        T Name { set; } // Solo setters.
+        void Set(T t);
+    }
+}
