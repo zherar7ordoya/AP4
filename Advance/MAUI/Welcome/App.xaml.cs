7200456d137717239c5d8659f9fc3ae71f4eb01d
zherar7ordoya <zherar7ordoya@outlook.com>
2022-03-17 04:13:30 -0300

    2022-03-17

---
+++ Advance/MAUI/Welcome/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Welcome;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Advance/MAUI/Welcome/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Welcome;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
