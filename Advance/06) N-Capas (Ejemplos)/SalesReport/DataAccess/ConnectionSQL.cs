93abe31832d08c1280531b756441c264414a8c66
zherar7ordoya <zherar7ordoya@outlook.com>
2022-03-21 12:53:02 -0300

    2022-03-21

---
+++ Advance/06) N-Capas (Ejemplos)/SalesReport/DataAccess/ConnectionSQL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataAccess
+{
+    public abstract class ConnectionSQL
+    {
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB; DataBase=Bike_Store; Integrated Security=True;");
+        }
+    }
+}
---
+++ Advance/06) N-Capas (Ejemplos)/SalesReport/Domain/SalesReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace Domain
+{
+    public class SalesReport
+    {
+        // Atributos & Propiedades
+        public DateTime reportDate { get; private set; }
+        public DateTime startDate { get; private set; }
+        public DateTime endDate { get; private set; }
+
+        // 14:00
+
+    }
+}
