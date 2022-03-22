d08786ef5a3178cb185138ea0f5d55d075edc585
zherar7ordoya <zherar7ordoya@outlook.com>
2022-03-22 12:55:17 -0300

    2022-03-22

--- Advance/06) N-Capas (Ejemplos)/SalesReport/Presentation/Form1.Designer.cs
+++ Advance/06) N-Capas (Ejemplos)/SalesReport/Presentation/Form1.Designer.cs
@@ -29,12 +29,59 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.salesReportBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.salesListingBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.netSalesByPeriodBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.salesReportBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salesListingBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.netSalesByPeriodBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.DataBindings.Add(new System.Windows.Forms.Binding("Tag", this.netSalesByPeriodBindingSource, "period", true));
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Presentation.Reports.SalesReport.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(38, 26);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(396, 246);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // salesReportBindingSource
+            // 
+            this.salesReportBindingSource.DataSource = typeof(Domain.SalesReport);
+            // 
+            // salesListingBindingSource
+            // 
+            this.salesListingBindingSource.DataSource = typeof(Domain.SalesListing);
+            // 
+            // netSalesByPeriodBindingSource
+            // 
+            this.netSalesByPeriodBindingSource.DataSource = typeof(Domain.NetSalesByPeriod);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.salesReportBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salesListingBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.netSalesByPeriodBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource netSalesByPeriodBindingSource;
+        private System.Windows.Forms.BindingSource salesReportBindingSource;
+        private System.Windows.Forms.BindingSource salesListingBindingSource;
     }
 }
 
--- Advance/06) N-Capas (Ejemplos)/SalesReport/Presentation/Form1.cs
+++ Advance/06) N-Capas (Ejemplos)/SalesReport/Presentation/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Domain;
 
 namespace Presentation
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1() => InitializeComponent();
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private void GetSalesReport(DateTime startDate, DateTime endDAte)
         {
-            InitializeComponent();
+            SalesReport reportModel = new SalesReport();
+            reportModel.CreateSalesOrderReport(startDate, endDAte);
+
+            salesReportBindingSource.DataSource = reportModel;
+            salesListingBindingSource.DataSource = reportModel.salesListing;
+            netSalesByPeriodBindingSource.DataSource = reportModel.netSalesByPeriod;
+
+            this.reportViewer1.RefreshReport();
+
+            //36:18
+
         }
     }
 }
