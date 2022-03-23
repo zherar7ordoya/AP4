679018d3d11da78f18bc630d60d3c55da538a670
zherar7ordoya <zherar7ordoya@outlook.com>
2022-03-23 19:13:53 -0300

    2022-03-23

--- Advance/06) N-Capas (Ejemplos)/SalesReport/DataAccess/OrderDAO.cs
+++ Advance/06) N-Capas (Ejemplos)/SalesReport/DataAccess/OrderDAO.cs
@@ -30,35 +30,43 @@ namespace DataAccess
                 using (var command = new SqlCommand())
                 {
                     command.Connection = connection;
-                    command.CommandText = @"
-                                            SELECT o.order_id,
-                                                   o.order_date,
-                                                   c.first_name + ', ' + c.last_name AS customer,
-                                                   products=Stuff((SELECT ' - ' + 'x'
-                                                                          + CONVERT(VARCHAR (10), oi2.quantity) + ' '
-                                                                          + product_name
-                                                                   FROM   Order_Items oi2
-                                                                          INNER JOIN products
-                                                                                  ON products.product_id = oi2.product_id
-                                                                   WHERE  oi2.order_id = oi1.order_id
-                                                                   FOR xml path('')), 1, 2, ''),
-                                                   Sum(( quantity * price ) - discount) AS total_amount
-                                            FROM   orders o
-                                                   INNER JOIN customers c
-                                                           ON c.customer_id = o.customer_id
-                                                   INNER JOIN Order_Items oi1
-                                                           ON oi1.order_id = o.order_id
-                                            WHERE  o.order_date BETWEEN @fromDate AND @toDate
-                                            GROUP  BY o.order_id,
-                                                      oi1.order_id,
-                                                      o.order_date,
-                                                      c.first_name,
-                                                      c.last_name
-                                            ORDER  BY o.order_id ASC 
-                                           ";
-                    command.Parameters.Add("@fromDate", SqlDbType.Date).Value = fromDate;
-                    command.Parameters.Add("@toDate", SqlDbType.Date).Value = toDate;
-                    command.CommandType = CommandType.Text;
+
+                    command.CommandText = @"GetSalesOrder";
+                    //command.CommandText = @"
+                    //                        SELECT o.order_id,
+                    //                               o.order_date,
+                    //                               c.first_name + ', ' + c.last_name AS customer,
+                    //                               products=Stuff((SELECT ' - ' + 'x'
+                    //                                                      + CONVERT(VARCHAR (10), oi2.quantity) + ' '
+                    //                                                      + product_name
+                    //                                               FROM   Order_Items oi2
+                    //                                                      INNER JOIN products
+                    //                                                              ON products.product_id = oi2.product_id
+                    //                                               WHERE  oi2.order_id = oi1.order_id
+                    //                                               FOR xml path('')), 1, 2, ''),
+                    //                               Sum(( quantity * price ) - discount) AS total_amount
+                    //                        FROM   orders o
+                    //                               INNER JOIN customers c
+                    //                                       ON c.customer_id = o.customer_id
+                    //                               INNER JOIN Order_Items oi1
+                    //                                       ON oi1.order_id = o.order_id
+                    //                        WHERE  o.order_date BETWEEN @fromDate AND @toDate
+                    //                        GROUP  BY o.order_id,
+                    //                                  oi1.order_id,
+                    //                                  o.order_date,
+                    //                                  c.first_name,
+                    //                                  c.last_name
+                    //                        ORDER  BY o.order_id ASC 
+                    //                       ";
+
+                    command.Parameters.AddWithValue("@fromDate", fromDate);
+                    //command.Parameters.Add("@fromDate", SqlDbType.Date).Value = fromDate;
+
+                    command.Parameters.AddWithValue("@toDate", toDate);
+                    //command.Parameters.Add("@toDate", SqlDbType.Date).Value = toDate;
+
+                    command.CommandType = CommandType.StoredProcedure;
+                    //command.CommandType = CommandType.Text;
 
                     var reader = command.ExecuteReader();
                     var table = new DataTable();
---
+++ Advance/06) N-Capas (Ejemplos)/SalesReport/SQL/Procedimiento Almacenado.sql
@@ -0,0 +1,34 @@
+USE BikeStore
+
+go
+
+CREATE PROC GetSalesOrder @fromDate DATE,
+                          @toDate   DATE
+AS
+    SELECT o.order_id,
+           o.order_date,
+           c.first_name + ', ' + c.last_name    AS customer,
+           products=Stuff((SELECT ' - ' + 'x'
+                                  + CONVERT(VARCHAR (10), oi2.quantity) + ' '
+                                  + product_name
+                           FROM   order_items oi2
+                                  INNER JOIN products
+                                          ON products.product_id =
+                                             oi2.product_id
+                           WHERE  oi2.order_id = oi1.order_id
+                           FOR xml path('')), 1, 2, ''),
+           Sum(( quantity * price ) - discount) AS total_amount
+    FROM   orders o
+           INNER JOIN customers c
+                   ON c.customer_id = o.customer_id
+           INNER JOIN order_items oi1
+                   ON oi1.order_id = o.order_id
+    WHERE  o.order_date BETWEEN @fromDate AND @toDate
+    GROUP  BY o.order_id,
+              oi1.order_id,
+              o.order_date,
+              c.first_name,
+              c.last_name
+    ORDER  BY o.order_id ASC
+
+go 
