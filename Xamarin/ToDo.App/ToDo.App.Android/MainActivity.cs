383188cfc76449b4cb1c358013e1dc2ece8bb74a
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-04-03 21:54:50 -0300

    2022-04-03

--- Xamarin/ToDo.App/ToDo.App.Android/MainActivity.cs
+++ Xamarin/ToDo.App/ToDo.App.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace ToDo.App.Droid
 {
-    [Activity(Label = "ToDo.App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "ToDo", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- Xamarin/ToDo.App/ToDo.App/Views/AddPage.xaml.cs
+++ Xamarin/ToDo.App/ToDo.App/Views/AddPage.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace ToDo.App
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDo.App.Views
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddPage : ContentPage
     {
-        public MainPage()
+        public AddPage()
         {
             InitializeComponent();
         }
