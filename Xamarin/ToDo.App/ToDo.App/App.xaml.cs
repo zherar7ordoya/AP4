5db03f38c98122879cae609d51b2d8a3b2f65fad
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-04-04 02:44:13 -0300

    2022-04-04

--- Xamarin/ToDo.App/ToDo.App/App.xaml.cs
+++ Xamarin/ToDo.App/ToDo.App/App.xaml.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using ToDo.App.Data;
 using ToDo.App.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.IO;
 
 namespace ToDo.App
 {
     public partial class App : Application
     {
+        public static DatabaseContext Context { get; set; }
+
         public App()
         {
             InitializeComponent();
-
+            InitializeDatabase();
             MainPage = new NavigationPage(new HomePage());
+        }
+
+        private void InitializeDatabase()
+        {
+            var folderApp = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var dbPath = Path.Combine(folderApp, "ToDo.db3");
+            Context = new DatabaseContext(dbPath);
         }
 
         protected override void OnStart()
---
+++ Xamarin/ToDo.App/ToDo.App/Data/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.App.Models;
+
+namespace ToDo.App.Data
+{
+    public class DatabaseContext
+    {
+        public SQLiteAsyncConnection Connection { get; set; }
+
+        public DatabaseContext(string path)
+        {
+            Connection = new SQLiteAsyncConnection(path);
+            Connection.CreateTableAsync<ToDoItem>().Wait();
+        }
+
+        public async Task<int> InsertItemAsync(ToDoItem item)
+        {
+            return await Connection.InsertAsync(item);
+        }
+    }
+}
