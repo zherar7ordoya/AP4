b83a628d8e4fb1554bc3ecc6fba99a744e4e714b
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-04-09 02:54:52 -0300

    2022-04-09

---
+++ Xamarin/ListBoxEnWPF/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListBoxEnWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Poblaciones> poblaciones = new List<Poblaciones>();
+
+            poblaciones.Add(new Poblaciones
+            {
+                Poblacion1 = "Madrid",
+                Poblacion2 = "Barcelona",
+                Temperatura1 = 15,
+                Temperatura2 = 17,
+                Diferencia = 3
+            });
+            poblaciones.Add(new Poblaciones
+            {
+                Poblacion1 = "Valencia",
+                Poblacion2 = "Alicane",
+                Temperatura1 = 19,
+                Temperatura2 = 20,
+                Diferencia = 6
+            });
+            poblaciones.Add(new Poblaciones
+            {
+                Poblacion1 = "Málaga",
+                Poblacion2 = "Bilbao",
+                Temperatura1 = 20,
+                Temperatura2 = 7,
+                Diferencia = -3
+            });
+            poblaciones.Add(new Poblaciones
+            {
+                Poblacion1 = "Sevilla",
+                Poblacion2 = "Coruña",
+                Temperatura1 = 22,
+                Temperatura2 = 8,
+                Diferencia = -6
+            });
+
+            ListaPoblaciones.ItemsSource = poblaciones;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListaPoblaciones.SelectedItem is null)
+            {
+                MessageBox.Show("Nada seleccionado", "Ups...", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                MessageBox.Show
+                    (
+                        (ListaPoblaciones.SelectedItem as Poblaciones).Poblacion1 + "\t" +
+                        (ListaPoblaciones.SelectedItem as Poblaciones).Temperatura1 + "Cº\n" +
+                        (ListaPoblaciones.SelectedItem as Poblaciones).Poblacion2 + "\t" +
+                        (ListaPoblaciones.SelectedItem as Poblaciones).Temperatura2 + "Cº"
+                    );
+            }
+        }
+
+        private void RowDefinition_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Button_Click(this, (RoutedEventArgs)EventArgs.Empty);
+        }
+
+        private void TextBlock_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Button_Click(this, null);
+        }
+    }
+}
---
+++ Xamarin/ListBoxEnWPF/Poblaciones.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListBoxEnWPF
+{
+    class Poblaciones
+    {
+        public string Poblacion1 { get; set; }
+        public int Temperatura1 { get; set; }
+        public string Poblacion2 { get; set; }
+        public int Temperatura2 { get; set; }
+        public int Diferencia { get; set; }
+    }
+}
