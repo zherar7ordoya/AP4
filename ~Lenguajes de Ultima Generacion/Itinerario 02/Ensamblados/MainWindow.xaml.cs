c9a007eebd82087e6b04a1c6d08cb9231fb35dc7
zherar7ordoya <zherar7ordoya@outlook.com>
2022-04-11 13:03:03 -0300

    2022-04-11

--- ~Lenguajes de Ultima Generacion/Itinerario 02/Ensamblados/MainWindow.xaml.cs
+++ ~Lenguajes de Ultima Generacion/Itinerario 02/Ensamblados/MainWindow.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using System.Windows;
+using Libreria;
 
 namespace Ensamblados
 {
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
-        private void Button_Click(object sender, RoutedEventArgs e) => MessageBox.Show("No he venido, amigos, a robar vuestros corazones.");
+        private void Button_Click(object sender, RoutedEventArgs e) => Cita.MostrarCita();
     }
 }
---
+++ ~Lenguajes de Ultima Generacion/Itinerario 02/Libreria/Cita.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Libreria
+{
+    public class Cita
+    {
+        public static void MostrarCita()
+        {
+            MessageBox.Show("No he venido, amigos, a robar vuestros corazones.");
+        }
+    }
+}
