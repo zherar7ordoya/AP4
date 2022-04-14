910850c2771af9b500f196012a383b6bb7e29cce
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-04-14 01:18:25 -0300

    2022-04-14

---
+++ Xamarin/Phoneword/Phoneword/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Phoneword
+{
+    // La clase App es de la que los proyectos principales crean una instancia
+    // y la que cargan.
+    public partial class App : Application
+    {
+        // El constructor de la clase App establece su propia propiedad
+        // MainPage. Esta propiedad controlará la primera pantalla que ve el
+        // usuario.
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
