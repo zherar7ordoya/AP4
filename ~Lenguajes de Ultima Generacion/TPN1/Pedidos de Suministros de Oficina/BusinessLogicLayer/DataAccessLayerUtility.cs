1b7384094fe4765784e4e63795eb724ab6c4968b
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-05-03 01:16:30 -0300

    2022-05-03

---
+++ ~Lenguajes de Ultima Generacion/TPN1/Pedidos de Suministros de Oficina/BusinessLogicLayer/DataAccessLayerUtility.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+
+namespace BusinessLogicLayer
+{
+    public static class DataAccessLayerUtility
+    {
+        public static string ObtenerConexionSQL(string nombre)
+        {
+            // Asumir fallo.
+            string respuesta = null;
+
+            // Buscar el nombre en la sección connectionStrings (App.config).
+            ConnectionStringSettings configuracion = ConfigurationManager.ConnectionStrings[nombre];
+
+            // Si se pudo encontrar, regresar la cadena de conexión.
+            if (configuracion != null) respuesta = configuracion.ConnectionString;
+
+            // Retorno.
+            return respuesta;
+        }
+    }
+    // TODO: 302
+    //TODO: connectionStrings (App.config)
+}
