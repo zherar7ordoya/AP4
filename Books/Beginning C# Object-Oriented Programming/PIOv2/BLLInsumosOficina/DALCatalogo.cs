5c89a20f51e0b747a5b97f983e46f588a946313e
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-05-09 01:12:31 -0300

    2022-05-09

---
+++ Books/Beginning C# Object-Oriented Programming/PIOv2/BLLInsumosOficina/DALCatalogo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BLLInsumosOficina
+{
+    public class DALCatalogo
+    {
+        public DataSet GetProductInfo()
+        {
+            DataSet _dsProducts;
+            string connString = DALConector.GetSQLConnection("ConexionConBBDD");
+            using (SqlConnection _conn = new SqlConnection(connString))
+            {
+                _dsProducts = new DataSet("Productos");
+                //Get category info
+                String strSQL =
+                    "SELECT ID_Categoria, Nombre, Descripcion " +
+                    "FROM Categorias";
+                using (SqlCommand cmdSelCategory = new SqlCommand(strSQL, _conn))
+                {
+                    using (SqlDataAdapter daCatagory = new SqlDataAdapter(cmdSelCategory))
+                    {
+                        daCatagory.Fill(_dsProducts, "Categorias");
+                    }
+                }
+
+                //Get product info
+                String strSQL2 =
+                    "SELECT ID_Producto, ID_Categoria, Nombre, Descripcion, Precio " +
+                    "FROM Productos";
+                using (SqlCommand cmdSelProduct = new SqlCommand(strSQL2, _conn))
+                {
+                    using (SqlDataAdapter daProduct = new SqlDataAdapter(cmdSelProduct))
+                    {
+                        daProduct.Fill(_dsProducts, "Productos");
+                    }
+                }
+            }
+            //Set up the table relation
+            DataRelation relacion = new DataRelation("relacion",
+                _dsProducts.Tables["Categorias"].Columns["ID_Categoria"],
+                _dsProducts.Tables["Productos"].Columns["ID_Categoria"], false);
+            _dsProducts.Relations.Add(relacion);
+            return _dsProducts;
+        }
+    }
+}
