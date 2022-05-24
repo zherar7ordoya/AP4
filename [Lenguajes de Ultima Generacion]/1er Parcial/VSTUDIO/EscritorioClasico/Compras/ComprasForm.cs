66edf0ea2f517dcb9601241e3c95aa84bd17969f
zherar7ordoya <zherar7ordoya@outlook.com>
2022-05-24 20:06:51 -0300

    2022-05-24

--- [Lenguajes de Ultima Generacion]/1er Parcial/VSTUDIO/EscritorioClasico/Compras/ComprasForm.cs
+++ [Lenguajes de Ultima Generacion]/1er Parcial/VSTUDIO/EscritorioClasico/Compras/ComprasForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Windows.Forms;
 
@@ -24,40 +25,169 @@ namespace EscritorioClasico.Compras
 
         private void ComprasForm_Load(object sender, EventArgs e) => InauguraForm();
 
-        readonly BLL.Cliente bllCliente = new BLL.Cliente();
+        BLL.Cliente bllCliente = new BLL.Cliente();
+        BEL.Cliente belCliente = new BEL.Cliente();
+
+        
 
         private void InauguraForm()
         {
-            this.ComprasDataGridView.DataSource = null;
-            this.ComprasDataGridView.Rows.Clear();
-            this.ComprasDataGridView.DataSource = bllCliente.Listar();
+            this.ClientesDataGridView.DataSource = null;
+            this.ClientesDataGridView.Rows.Clear();
+            this.ClientesDataGridView.DataSource = bllCliente.Listar();
         }
 
 
 
-        private void AltaCompraButton_Click(object sender, EventArgs e)
+        
+
+        private void ComprasDataGridView_MouseClick(object sender, MouseEventArgs e)
         {
-            // Instancio el Form ABM y le paso los datos a editar.
-            CompraForm formulario = new CompraForm();
-
-            formulario.CodigoTextBox.Text = belCliente.Codigo.ToString();
-            formulario.NombreTextBox.Text = belCliente.Nombre.ToString();
-            formulario.ApellidoTextBox.Text = belCliente.Apellido.ToString();
-            formulario.DNITextBox.Text = belCliente.DNI.ToString();
-            formulario.FechaVencimientoDateTimePicker.Value = belCliente.FechaNacimiento;
-
-            // Si la respuesta es OK, actualizo la grilla.
-            DialogResult respuesta = formulario.ShowDialog();
-            if (respuesta == DialogResult.OK) CargaInicial();
-
-
-
-
-
-            CompraForm formulario = CompraForm.Instancia();
-            DialogResult respuesta = formulario.ShowDialog();
-            if (respuesta == DialogResult.OK) MessageBox.Show("DialogResult: OK");
+            belCliente = (BEL.Cliente)DataGridView_Clientes.CurrentRow.DataBoundItem;
+            AsignarTarjetaATextBox(belCliente);
         }
+        private void AsignarTarjetaATextBox(BEL.Cliente ClieAux)
+        {
+            BEL.Cliente ClieAux2 = bllCliente.Detallar(ClieAux);
+            BEL.Tarjeta oBETarjAux = TarjetaDeAlta(ClieAux2);
+            if (oBETarjAux != null)
+            {
+                TextBox_Numero_Tarjeta.Text = oBETarjAux.Numero.ToString();
+                TextBox_Saldo_Tarjeta.Text = oBETarjAux.Saldo.ToString();
+            }
+            else
+            {
+                TextBox_Numero_Tarjeta.Text = "NO";
+                TextBox_Saldo_Tarjeta.Text = "NO";
+            }
+        }
+
+
+
+        private BEL.Tarjeta TarjetaDeAlta(BEL.Cliente ClieAux)
+        {
+            BEL.Tarjeta oBETarjAux = null;
+            if (ClieAux.Tarjeta != null)
+            {
+                foreach (BEL.Tarjeta Tarj in ClieAux.Tarjeta)
+                {
+                    if (Tarj.Estado == "Alta")
+                    {
+                        oBETarjAux = Tarj;
+                    }
+                }
+            }
+            return oBETarjAux;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        BEL.TarjetaInternacional belGiftcardInternacional = new BEL.TarjetaInternacional();
+        BEL.TarjetaNacional belGiftcardNacional = new BEL.TarjetaNacional();
+
+        BLL.TarjetaInternacional bllGiftcardInternacional = new BLL.TarjetaInternacional();
+        BLL.TarjetaNacional bllGiftcardNacional = new BLL.TarjetaNacional();
+
+        private void RegistrarButton_Click(object sender, EventArgs e)
+        {
+            belGiftcardInternacional = null;
+            belGiftcardNacional = null;
+            BEL.Cliente belClienteAux = (BEL.Cliente)DataGridView_Clientes.CurrentRow.DataBoundItem;
+            belCliente = bllCliente.Detallar(belCliente);
+            BEL.Tarjeta oBEtarjeta = TarjetaDeAlta(belCliente);
+            List<BEL.TarjetaInternacional> ListaTarjetasInt = bllGiftcardInternacional.Listar();
+            List<BEL.TarjetaNacional> ListaTarjetasNac = bllGiftcardNacional.Listar();
+
+            int MontoCompra = Convert.ToInt32(TextBox_Monto_compra.Text);
+            int MontoTarjeta = Convert.ToInt32(TextBox_Saldo_Tarjeta.Text);
+
+            if (oBEtarjeta != null)
+            {
+                foreach (BEL.TarjetaInternacional belGiftcardInternacionalAux in ListaTarjetasInt)
+                {
+                    if (oBEtarjeta.Codigo == belGiftcardInternacionalAux.Codigo)
+                    {
+                        belGiftcardInternacional = belGiftcardInternacionalAux;
+                    }
+                }
+                foreach (BEL.TarjetaNacional belGiftcardNacionalAux in ListaTarjetasNac)
+                {
+                    if (oBEtarjeta.Codigo == belGiftcardNacionalAux.Codigo)
+                    {
+                        belGiftcardNacional = belGiftcardNacionalAux;
+                    }
+                }
+                if (MontoCompra <= MontoTarjeta)
+                {
+                    if (belGiftcardInternacional != null)
+                    {
+                        oBEDesc.DescuentoOtorgado = bllGiftcardInternacional.ObtenerDescuento(MontoCompra);
+                        oBEDesc.NumeroTarjeta = belGiftcardInternacional.Numero;
+                        oBEDesc.Tipo = "Internacional";
+                        oBLDesc.Guardar(oBEDesc);
+
+                        belGiftcardInternacional.Saldo = belGiftcardInternacional.Saldo - MontoCompra;
+                        if (belGiftcardInternacional.Saldo == 0)
+                        {
+                            belGiftcardInternacional.Estado = "Sin Saldo";
+                        }
+                        bllGiftcardInternacional.Guardar(belGiftcardInternacional);
+                    }
+                    else if (belGiftcardNacional != null)
+                    {
+                        oBEDesc.DescuentoOtorgado = bllGiftcardNacional.ObtenerDescuento(MontoCompra);
+                        oBEDesc.NumeroTarjeta = belGiftcardNacional.Numero;
+                        oBEDesc.Tipo = "Nacional";
+                        oBLDesc.Guardar(oBEDesc);
+                        belGiftcardNacional.Saldo = belGiftcardNacional.Saldo - MontoCompra;
+                        if (belGiftcardNacional.Saldo == 0)
+                        {
+                            belGiftcardNacional.Estado = "Sin Saldo";
+                        }
+                        bllGiftcardNacional.Guardar(belGiftcardNacional);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No hay saldo suficiente");
+                }
+            }
+            else
+            {
+                MessageBox.Show("El cliente no tiene tarjeta dada de alta");
+            }
+            Limpiar();
+        }
+
+
+
+        void Limpiar()
+        {
+            List<TextBox> ListTxtBox = new List<TextBox>();
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is TextBox)
+                {
+                    ((TextBox)ctrl).Text = String.Empty;
+                }
+            }
+        }
+
+
+
 
     }
 }
