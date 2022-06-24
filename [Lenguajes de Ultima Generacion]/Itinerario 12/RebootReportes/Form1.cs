436d784829d0743f0cebc979c7ffb687d9f6c47e
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-06-24 01:55:55 -0300

    2022-06-24

--- [Lenguajes de Ultima Generacion]/Itinerario 12/RebootReportes/Form1.cs
+++ [Lenguajes de Ultima Generacion]/Itinerario 12/RebootReportes/Form1.cs
@@ -16,5 +16,11 @@ namespace RebootReportes
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
     }
 }
