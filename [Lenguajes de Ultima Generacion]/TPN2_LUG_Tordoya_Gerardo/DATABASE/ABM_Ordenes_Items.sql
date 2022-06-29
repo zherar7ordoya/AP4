04685a51f35b7a6fe9af0999f06a79c682d9e24a
zherar7ordoya <zherar7ordoya@outlook.com>
2022-06-29 19:18:59 -0300

    2022-06-29

--- [Lenguajes de Ultima Generacion]/TPN2_LUG_Tordoya_Gerardo/DATABASE/ABM_Ordenes_Items.sql
+++ [Lenguajes de Ultima Generacion]/TPN2_LUG_Tordoya_Gerardo/DATABASE/ABM_Ordenes_Items.sql
@@ -7,11 +7,22 @@ USE PedidosDeInsumosDeOficina
 GO
 
 CREATE PROCEDURE ABM_Ordenes_Items
-    @Id          INTEGER,
-    @OrdenID     INTEGER,
-    @ItemId      INTEGER,
-    @Cantidad    INTEGER,
-    @Instruccion CHAR(6)
+    --Ordenes
+	@Legajo         INTEGER,
+	@Fecha          DATE,
+	@Estado         CHAR(10),
+	--Items
+	@CategoriaID    CHAR(4),
+	@Nombre         VARCHAR(MAX),
+	@PrecioUnitario DECIMAL(9, 2),
+	@ProveedorID    CHAR(4),
+	--Ordenes_Items
+	@Id             INTEGER,
+    @OrdenID        INTEGER,
+    @ItemID         INTEGER,
+    @Cantidad       INTEGER,
+	--Procedimiento Almacenado
+    @Instruccion    CHAR(6)
 AS
 BEGIN
 
@@ -19,78 +30,74 @@ BEGIN
     DECLARE @ID_ITEM int
 
     --||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| ALTAS
-    --AYUDA-MEMORIA:
-    --Students => Ordenes (Pedidos)
-    --Courses  => Items   (Productos)
-
 	--NOTA:
 	--Sin calibrar. El uso pensado es dar de alta una orden y agregar ítems
 	--a una orden. No se han hecho más pruebas.
-
 	--Parámetros necesarios: creo que todos.
 
     IF @Instruccion = 'Insert'
     BEGIN
-        INSERT Ordenes (StudentName)
-        SELECT DISTINCT @StudentName
-        FROM Ordenes
-        WHERE NOT EXISTS
-        (
-            SELECT * 
-			FROM Ordenes 
-			WHERE OrdenID = @OrdenID
-        )
 
-        IF @OrdenID = 0
+	    --Busco por empleado que pide (por el momento, hasta mejorar el diseño).
+        SELECT @ID_ORDEN = OrdenID
+	    FROM Ordenes
+	    WHERE Legajo = @Legajo
+
+	    --Busco por nombre del producto (por el momento, hasta mejorar el diseño).
+	    SELECT @ID_ITEM = ItemID
+	    FROM Items
+	    WHERE Nombre = @Nombre
+
+        IF (@ID_ORDEN IS NULL)
         BEGIN
+            INSERT INTO Ordenes
+			(
+			    Legajo,
+				Fecha,
+				Estado
+			)
+		    VALUES
+			(
+			    @Legajo,
+				@Fecha,
+				@Estado
+			)
             SELECT @ID_ORDEN = SCOPE_IDENTITY()
         END
-        ELSE
-        BEGIN
-            SELECT @ID_ORDEN = @OrdenID
-        END
 
-        INSERT Items (CourseName)
-        SELECT DISTINCT @CourseName
-        FROM Items
-        WHERE NOT EXISTS
-        (
-            SELECT * 
-			FROM Items 
-			WHERE ItemID = @ItemID
-        )
-
-        IF @ItemID = 0
+        IF (@ID_ITEM IS NULL)
         BEGIN
+            INSERT INTO Items
+			(
+			    CategoriaID,
+				Nombre,
+				PrecioUnitario,
+				ProveedorID
+			)
+		    VALUES
+			(
+			    @CategoriaID,
+				@Nombre,
+				@PrecioUnitario,
+				@ProveedorID
+			)
             SELECT @ID_ITEM = SCOPE_IDENTITY()
         END
-        ELSE
-        BEGIN
-            SELECT @ID_ITEM = @ItemID
-        END
 
-        INSERT INTO Ordenes_Items
-        VALUES (@ID_ORDEN, @ID_ITEM)
+	    INSERT INTO Ordenes_Items
+        VALUES (@ID_ORDEN, @ID_ITEM, @Cantidad)
+
     END
 
-
-
-
     --||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| BAJAS
-    --AYUDA-MEMORIA:
-    --Students => Ordenes (Pedidos)
-    --Courses  => Items   (Productos)
-
 	--USO: 
 	--Las tablas son de órdenes y sus ítems.
 	--Si deseo borrar la órden completa, proveo su ID.
 	--POR EL MOMENTO, ES TODA LA OPERACIÓN QUE SE HARÁ.
-
 	--EN UN FUTURO, SE PODRÍA AJUSTAR PARA QUE:
 	--Si no deseo borrar la órden, Id = 0
 	--Si deseo borrar un ítem de la orden, proveo su ID.
 	--Si no deseo borrar un ítem, Id = 0
-
 	--Parámetros necesarios: ID de la órden.
 
     IF @Instruccion = 'Delete'
@@ -99,28 +106,18 @@ BEGIN
         WHERE OrdenID = @OrdenID
     END
 
-
-
-
     --|||||||||||||||||||||||||||||||||||||||||||||||||||||||||| MODIFICACIONES
-    --AYUDA-MEMORIA:
-    --Students => Ordenes (Pedidos)
-    --Courses  => Items   (Productos)
-
     --NOTA:
 	--Update, por el momento, pienso que solo puede ser utilizado
 	--para cambiar, por ejemplo, la cantidad de artículos pedida.
-
 	--Parámetros necesarios: ID del ítem.
 
 	IF @Instruccion = 'Update'
     BEGIN
-        UPDATE Items
-        SET CourseName = @CourseName
-        WHERE ItemID = @ItemID
+        UPDATE Ordenes_Items
+        SET Cantidad = @Cantidad
+        WHERE OrdenID = @OrdenID AND ItemID = @ItemID
     END
-
-
 
     --|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| LISTADOS
 	--Por el momento (incluso creo que esta sección es un "huevo de pascua")
@@ -142,7 +139,5 @@ BEGIN
 			ON Ordenes_Items.OrdenID = Ordenes.OrdenID
         WHERE Ordenes.OrdenID = @OrdenID
     END
-
-
 
 END
