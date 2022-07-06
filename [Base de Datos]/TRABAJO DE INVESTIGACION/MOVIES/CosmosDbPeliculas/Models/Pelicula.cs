6029541b69625932f7eef103ad6838cf92f49169
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-07-06 02:21:50 -0300

    2022-07-06

---
+++ [Base de Datos]/TRABAJO DE INVESTIGACION/MOVIES/CosmosDbPeliculas/Models/Pelicula.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmosDbPeliculas.Models
+{
+    public class Pelicula
+    {
+        [JsonProperty(PropertyName = "id")]
+        public String Id { get; set; }
+        public String Categoria { get; set; }
+        public String Titulo { get; set; }
+        public String Director { get; set; }
+        public String Estreno { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
