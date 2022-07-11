02b11b15f0db4d09149a50f9bd6a961c2b8d9a08
zherar7ordoya <zherar7ordoya@outlook.com>
2022-07-11 12:48:04 -0300

    2022-07-11

---
+++ [Lenguajes de Ultima Generacion]/Itinerario 15 (Parcial 2)/Piedra-Papel-Tijera/FormPPT.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace Piedra_Papel_Tijera
+{
+    partial class FormPPT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Piedra = new System.Windows.Forms.Button();
+            this.Papel = new System.Windows.Forms.Button();
+            this.Tijera = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Piedra_Papel_Tijera.Properties.Resources.pregunta;
+            this.pictureBox1.Location = new System.Drawing.Point(164, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Piedra_Papel_Tijera.Properties.Resources.pregunta;
+            this.pictureBox2.Location = new System.Drawing.Point(479, 70);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Piedra
+            // 
+            this.Piedra.Location = new System.Drawing.Point(12, 12);
+            this.Piedra.Name = "Piedra";
+            this.Piedra.Size = new System.Drawing.Size(75, 23);
+            this.Piedra.TabIndex = 2;
+            this.Piedra.Text = "Piedra";
+            this.Piedra.UseVisualStyleBackColor = true;
+            this.Piedra.Click += new System.EventHandler(this.Piedra_Click);
+            // 
+            // Papel
+            // 
+            this.Papel.Location = new System.Drawing.Point(12, 41);
+            this.Papel.Name = "Papel";
+            this.Papel.Size = new System.Drawing.Size(75, 23);
+            this.Papel.TabIndex = 3;
+            this.Papel.Text = "Papel";
+            this.Papel.UseVisualStyleBackColor = true;
+            this.Papel.Click += new System.EventHandler(this.Papel_Click);
+            // 
+            // Tijera
+            // 
+            this.Tijera.Location = new System.Drawing.Point(12, 70);
+            this.Tijera.Name = "Tijera";
+            this.Tijera.Size = new System.Drawing.Size(75, 23);
+            this.Tijera.TabIndex = 4;
+            this.Tijera.Text = "Tijera";
+            this.Tijera.UseVisualStyleBackColor = true;
+            this.Tijera.Click += new System.EventHandler(this.Tijera_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(534, 337);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Jugador";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(522, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "CPU";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(350, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(350, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 347);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(291, 347);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "label6";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Cronometro_Tick);
+            // 
+            // FormPPT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.Tijera);
+            this.Controls.Add(this.Papel);
+            this.Controls.Add(this.Piedra);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormPPT";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Piedra-Papel-Tijera";
+            this.Load += new System.EventHandler(this.FormPPT_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button Piedra;
+        private System.Windows.Forms.Button Papel;
+        private System.Windows.Forms.Button Tijera;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ [Lenguajes de Ultima Generacion]/Itinerario 15 (Parcial 2)/Piedra-Papel-Tijera/FormPPT.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Piedra_Papel_Tijera
+{
+    public partial class FormPPT : Form
+    {
+        //declaring the variables for this game
+
+        public int rounds = 3; // 3 rounds per game
+        public int timePerRound = 6; // 5 seconds per rounds
+        //enemy choice options stored inside an array for easy access
+        string[] AIchoice = { "rock", "paper", "scissor", "rock", "scissor", "paper" };
+        public int randomNumber;
+        string command;
+        Random rnd = new Random();
+        string playerChoice;
+        int playerWins = 0;
+        int AIwins = 0;
+
+        // end of declaring variables
+
+
+        public FormPPT()
+        {
+            InitializeComponent();
+        }
+        private void FormPPT_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            playerChoice = "none";
+        }
+
+        private void Cronometro_Tick(object sender, EventArgs e)
+        {
+
+
+
+
+
+
+            label6.Text = Convert.ToString(rounds);
+
+            timePerRound--; // reduce the time by 1
+            label4.Text = Convert.ToString(timePerRound); // show the time on the screen
+            if (timePerRound < 1) // if the time is less then one second 
+            {
+
+                timer1.Enabled = false; // we disable the timer if less then one second left 
+
+                timePerRound = 6; // set the timer back to 6 seconds 
+                randomNumber = rnd.Next(0, 5); // randomize the number again 
+                command = AIchoice[randomNumber];
+                // we set up the AI choice according to the random number 
+
+                // the switch statement below will show the AI choice and change the picture box images 
+
+                switch (command)
+                {
+                    case "rock":
+                        pictureBox2.Image = Properties.Resources.piedra;
+                        break;
+
+                    case "paper":
+                        pictureBox2.Image = Properties.Resources.papel;
+                        break;
+
+                    case "scissor":
+                        pictureBox2.Image = Properties.Resources.tijera;
+                        break;
+
+                    default:
+                        break;
+
+                }
+
+                // if we have more rounds to the play then we run the check game function 
+
+                if (rounds > 1)
+                {
+                    checkGame();
+                }
+                // if we dont have any more rounds to play then we go to the final decision engine
+                else
+                {
+                    decisionEngine();
+                }
+
+            }
+        }
+
+
+
+
+        private void checkGame()
+        {
+            if (playerChoice == "rock" && command == "paper")
+            {
+                MessageBox.Show("AI Wins");
+                AIwins++;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "paper" && command == "rock")
+            {
+                MessageBox.Show("player Wins");
+                playerWins++;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "paper" && command == "scissor")
+            {
+                MessageBox.Show("AI Wins");
+                AIwins++;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "scissor" && command == "paper")
+            {
+                MessageBox.Show("player Wins");
+                playerWins++;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "scissor" && command == "rock")
+            {
+                MessageBox.Show("AI Wins");
+                AIwins++;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "rock" && command == "scissor")
+            {
+                MessageBox.Show("player Wins");
+                playerWins++;
+                rounds--;
+                nextRound();
+            }
+            else if (playerChoice == "none")
+            {
+                MessageBox.Show(label1.Text + " Make a choice");
+                nextRound();
+            }
+            else
+            {
+                MessageBox.Show("Draw");
+                nextRound();
+            }
+        }
+
+
+
+
+        private void decisionEngine()
+        {
+            if (playerWins > AIwins)
+            {
+                label3.Text = label1.Text + " Wins the game";
+            }
+            else
+            {
+                label3.Text = "AI Wins the game";
+            }
+        }
+
+
+
+        private void nextRound()
+        {
+            playerChoice = "none";
+            pictureBox1.Image = Properties.Resources.pregunta;
+            timer1.Enabled = true;
+            pictureBox2.Image = Properties.Resources.pregunta;
+
+        }
+
+        private void Piedra_Click(object sender, EventArgs e)
+        {
+            playerChoice = "rock";
+            pictureBox1.Image = Properties.Resources.piedra;
+        }
+
+        private void Papel_Click(object sender, EventArgs e)
+        {
+            playerChoice = "paper";
+            pictureBox1.Image = Properties.Resources.papel;
+        }
+
+        private void Tijera_Click(object sender, EventArgs e)
+        {
+            playerChoice = "scissor";
+            pictureBox1.Image = Properties.Resources.tijera;
+        }
+    }
+}
