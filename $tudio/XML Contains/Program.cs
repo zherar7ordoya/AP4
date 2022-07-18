88f45cba79c1b4c827d77b1d31367da6a2797349
zherar7ordoya <zherar7ordoya@outlook.com>
2022-07-18 13:03:30 -0300

    2022-07-18

---
+++ $tudio/XML Contains/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace XML_Contains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xDocSload = XDocument.Load(Directory.GetCurrentDirectory() + @"\series.xml");
+
+            var sList = xDocSload.Root.Elements("series").Select(element => element.Value).ToList();
+            
+            foreach (var x in sList)
+            {
+                if (x.Contains("Tordoya")) Console.WriteLine(x);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
