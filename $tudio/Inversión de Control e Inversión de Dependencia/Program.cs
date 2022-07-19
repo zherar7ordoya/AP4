e588c60a79f69ca13ba62987e3675fda8e44bcb4
zherar7ordoya <zherar7ordoya@outlook.com>
2022-07-19 10:03:44 -0300

    2022-07-19

---
+++ $tudio/Inversión de Control e Inversión de Dependencia/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inversión_de_Control_e_Inversión_de_Dependencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
