3418f2e4b803f03d3b277064cee83070c63275a0
zherar7ordoya <zherar7ordoya@outlook.com>
2022-07-25 19:56:23 -0300

    2022-07-25

---
+++ $tudio/SOLID (Tim Corey)/Video2OConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Video2OLibrary;
+
+namespace Video2OConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<PersonModel> applicants = new List<PersonModel>
+
+            {
+                new PersonModel{FirstName="Tim", LastName="Corey"},
+                new PersonModel{FirstName="Sue", LastName="Storm", TypeOfEmployee=EmployeeType.Manager},
+                new PersonModel{FirstName="Nancy", LastName="Roman", TypeOfEmployee=EmployeeType.Executive},
+            };
+
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            Accounts accountProcessor = new Accounts();
+
+            foreach (var person in applicants)
+            {
+                employees.Add(accountProcessor.Create(person));
+            }
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine(
+                    $"{emp.FirstName} " +
+                    $"{emp.LastName}: " +
+                    $"{emp.EmailAddress} " +
+                    $"\tIsManager: {emp.IsManager} " +
+                    $"\tIsExecutive: {emp.IsExecutive}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ $tudio/SOLID (Tim Corey)/Video2OLibrary/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Video2OLibrary
+{
+    public enum EmployeeType
+    {
+        Staff,
+        Manager,
+        Executive
+    }
+}
