4ec7620eee37e26a8e5dbd6234ebe58eb349c2e3
zherar7ordoya <zherar7ordoya@outlook.com>
2022-08-11 19:57:36 -0300

    2022-08-11

---
+++ $tudio/Callback/ByDelegate/Program.cs
@@ -0,0 +1,70 @@
+﻿/** https://www.c-sharpcorner.com/UploadFile/1c8574/delegate-used-for-callback-operation/
+ * 
+ * A "callback" is a term that refers to a coding design pattern. In this
+ * design pattern, executable code is passed as an argument to other code and 
+ * it is expected to call back at some time. This callback can be synchronous
+ * or asynchronous. So, in this way, large piece of the internal behavior of a 
+ * method from the outside of a method can be controlled. It is basically a 
+ * function pointer that is being passed into another function.
+ * 
+ * Delegate is a famous way to implement Callback in C#.
+ * But, it can also be implemented by Interface.
+ */
+
+using static System.Console;
+
+/// <summary>
+/// Delegate provides a way to pass a method as argument to other method. To
+/// create a Callback in C#, function address will be passed inside a variable.
+/// So, this can be achieved by using Delegate.
+/// 
+/// NOTA:
+/// If you need more than one Callback method, then callback mechanism with the
+/// use of delegate doesn't makes sense.
+/// </summary>
+namespace ByDelegate
+{
+    class Program
+    {
+        static void Main()
+        {
+            CLASE_CallbackTest callbackTest = new CLASE_CallbackTest();
+            callbackTest.Test();
+            ReadLine();
+        }
+    }
+
+    //=========================================================================
+
+    public delegate void DELEGADO(string resultado);
+
+    public class CLASE_Callback
+    {
+        public void StartNewTask(DELEGADO callback)
+        {
+            WriteLine("I have started a new task.");
+            // if (taskCompletedCallBack != null) taskCompletedCallBack("I have completed Task.");
+            callback?.Invoke("I have completed the task.");
+        }
+    }
+
+    //=========================================================================
+
+    public class CLASE_CallbackTest
+    {
+        public void Test()
+        {
+            DELEGADO callback = TestCallback;
+            CLASE_Callback testCallback = new CLASE_Callback();
+            testCallback.StartNewTask(callback);
+        }
+
+        public void TestCallback(string result)
+        {
+            WriteLine(result);
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+
+}
---
+++ $tudio/Callback/ByInterface/Program.cs
@@ -0,0 +1,99 @@
+﻿/** https://www.c-sharpcorner.com/UploadFile/1c8574/delegate-used-for-callback-operation/
+ * 
+ * A "callback" is a term that refers to a coding design pattern. In this
+ * design pattern, executable code is passed as an argument to other code and 
+ * it is expected to call back at some time. This callback can be synchronous
+ * or asynchronous. So, in this way, large piece of the internal behavior of a 
+ * method from the outside of a method can be controlled. It is basically a 
+ * function pointer that is being passed into another function.
+ * 
+ * Delegate is a famous way to implement Callback in C#.
+ * But, it can also be implemented by Interface.
+ */
+
+using static System.Console;
+
+/// <summary>
+/// Delegate is a good way to implement Callback. But, you could use interfaces
+/// for this. Suppose you have two methods (one for the success and another for
+/// error) and these methods will use callback. If you will use delegates, you
+/// will have to take two delegates.
+/// If you need more than one callback method, then use of delegates doesn't
+/// makes sense. The use of interfaces provides flexible and well-performing
+/// callback mechanisms for this scenario.
+/// </summary>
+namespace ByInterface
+{
+    class Program
+    {
+        static void Main()
+        {
+            MeetingExecution meetingExecution = new MeetingExecution();
+            meetingExecution.PerformMeeting();
+
+            ReadLine();
+        }
+    }
+
+    //=========================================================================
+
+    public interface IMeeting
+    {
+        void ShowAgenda(string agenda);
+        void EmployeeAttendedMeeting(string employee);
+        void EmployeeLeftMeeting(string employee);
+    }
+
+    //=========================================================================
+
+    public class Meeting : IMeeting
+    {
+        public void ShowAgenda(string agenda)
+        {
+            WriteLine("Agenda Details: " + agenda);
+        }
+
+        public void EmployeeAttendedMeeting(string employee)
+        {
+            WriteLine("Employee Attended Meeting: " + employee);
+        }
+
+        public void EmployeeLeftMeeting(string employee)
+        {
+            WriteLine("Employee Left Meeting: " + employee);
+        }
+    }
+
+    //=========================================================================
+
+    public class MeetingRoom
+    {
+        private string message;
+        public MeetingRoom(string message)
+        {
+            this.message = message;
+        }
+
+        public void StartMeeting(IMeeting meeting)
+        {
+            // Its a callback
+            if (meeting != null) meeting.ShowAgenda(message);
+
+        }
+    }
+
+    //=========================================================================
+
+    public class MeetingExecution
+    {
+        public void PerformMeeting()
+        {
+            IMeeting meeging = new Meeting();
+            MeetingRoom meetingStarted = new MeetingRoom("Code Quality Improvement.");
+            meetingStarted.StartMeeting(meeging);
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+
+}
